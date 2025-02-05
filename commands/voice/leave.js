cbe253c6daea2d59661ee539685afdea50ca2c70
Sxwako <110779036+Sxwako@users.noreply.github.com>
2025-02-05 14:10:17 +0900

    add: When "/leave" is received, disconnect the VC and send a warning message if the user is not in the VC or BOT is not in the VC.
    - add "leave.js"

---
+++ commands/voice/leave.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, Events } = require("discord.js");
+const { getVoiceConnection } = require("@discordjs/voice");
+
+module.exports = {
+	name: Events.InteractionCreate,
+	once: false,
+	data: new SlashCommandBuilder()
+		.setName("leave")
+		.setDescription("VCから退出します"),
+
+	async execute(interaction) {
+		const connection = getVoiceConnection(interaction.guildId);
+		const guild = interaction.guild;
+		const member = await guild.members.fetch(interaction.member.id);
+		const channel = member.voice.channel;
+		if (!channel) {
+			return interaction.reply(":cold_sweat: VCに参加してから実行してください");
+		}
+		else if (!connection) {
+			return interaction.reply(":thinking: BOTは現在VCに参加していません");
+		}
+		else if (connection) {
+			connection.destroy();
+			return interaction.reply(":wave: VCから切断しました!");
+		}
+		else {return await interaction.reply("An unexpected error occurred.");}
+	},
+};
