f8e757bc4d952ff86ba30b2dfecef23fea873788
Sxwako <110779036+Sxwako@users.noreply.github.com>
2025-02-10 01:09:05 +0900

    improve: send text message when command execute

--- commands/misc/google.js
+++ commands/misc/google.js
@@ -1,8 +1,10 @@
 const { SlashCommandBuilder } = require("discord.js");
-const { data } = require("../utility/ping");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("google")
-    .setDescription("Google検索をします"),
-}
+	data: new SlashCommandBuilder()
+		.setName("google")
+		.setDescription("Google検索をします"),
+	async execute(interaction) {
+		await interaction.reply("ggrks");
+	},
+};
