97f745e05b8d10c2c3e86ad0e8e305c46ff29876
Sxwako <110779036+Sxwako@users.noreply.github.com>
2025-02-10 00:15:42 +0900

    improve: add library "axios" and each process is contained in a function

--- voice/tts/generateAudio.js
+++ voice/tts/generateAudio.js
@@ -1,4 +1,5 @@
 const fs = require("node:fs");
+const { default: axios } = require("axios");
 const rpc = axios.create({ baseURL: "http://voicevox-engine:50021/", proxy: false });
 
 
@@ -18,16 +19,19 @@ module.exports = async function readMessages(message) {
 
 	// ここまで 旧readMessages.jsの内容
 	// ここから 旧generateAudio.jsの内容
-	const audioQuery = await rpc.post(`audio_query?text=${encodeURI(convMessage)}&speaker=${voice}, ${{
-		headers: { "accept" : "application/json" },
-	}}`);
+	async function generateAudio(text, filePath, speakerName) {
+		const audioQuery = await rpc.post(`audio_query?text=${encodeURI(text)}&speaker=${speakerName}`, {
+			headers: { "accept": "application/json" },
+		});
 
-	const synthesis = await rpc.post(`synthesis?speaker=${voice}, ${JSON.stringify(audioQuery.data)}, ${{
-		responseType: "arraybuffer",
-		headers: {
-			"accept" : "audio/wav",
-			"Content-Type" : "appication/json",
-		},
-	}}`);
-	fs.writeFileSync(soundPath, new Buffer.from(synthesis.data), "binary");
+		const synthesis = await rpc.post(`synthesis?speaker=${speakerName}`, JSON.stringify(audioQuery.data), {
+			responseType: "arraybuffer",
+			headers: {
+				"accept": "audio/wav",
+				"Content-Type": "appication/json",
+			},
+		});
+
+		fs.writeFileSync(filePath, new Buffer.from(synthesis.data), "binary");
+	}
 };
