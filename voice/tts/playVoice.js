d07b3c40bee43c76f8254914129ab4b1f759a00d
kasumi-sou <110779036+Sxwako@users.noreply.github.com>
2025-02-11 01:21:28 +0900

    feat: read messages sequentially

--- voice/tts/playVoice.js
+++ voice/tts/playVoice.js
@@ -1,15 +1,28 @@
 const { createAudioResource, StreamType, createAudioPlayer, NoSubscriberBehavior, getVoiceConnection, entersState, AudioPlayerStatus } = require("@discordjs/voice");
-
-
+const { LockObj, lock } = require("@mtripg6666tdr/async-lock");
+// const locker = new LockObj();
+const guildLocker = new Map();
+function getLockerByGuildId(guildId) {
+	if (guildLocker.has(guildId)) {
+		return guildLocker.get(guildId);
+	}
+	else {
+		const locker = new LockObj;
+		guildLocker.set(guildId, locker);
+		return locker;
+	}
+}
 module.exports = async function play(filePath, guildId) {
-	const connection = getVoiceConnection(guildId);
-	const resource = createAudioResource(filePath, { inputType: StreamType.Arbitrary });
-	const player = createAudioPlayer({
-		behaviors: {
-			noSubscriber: NoSubscriberBehavior.Pause,
-		},
+	await lock(getLockerByGuildId(guildId), async () => {
+		const connection = getVoiceConnection(guildId);
+		const resource = createAudioResource(filePath, { inputType: StreamType.Arbitrary });
+		const player = createAudioPlayer({
+			behaviors: {
+				noSubscriber: NoSubscriberBehavior.Pause,
+			},
+		});
+		connection.subscribe(player);
+		player.play(resource);
+		await entersState(player, AudioPlayerStatus.Idle, 30000);
 	});
-	connection.subscribe(player);
-	player.play(resource);
-	await entersState(player, AudioPlayerStatus.Idle, 30000);
 };
