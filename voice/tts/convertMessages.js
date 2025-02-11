2dec68c0e44c0c262277d0ff016a96a18dbd5884
kasumi-sou <110779036+Sxwako@users.noreply.github.com>
2025-02-12 01:05:26 +0900

    feat: convert messages when user sent code block messages

--- voice/tts/convertMessages.js
+++ voice/tts/convertMessages.js
@@ -1,4 +1,32 @@
 module.exports = function(messageContent) {
+	messageContent = messageContent.toLowerCase();
+	if (messageContent.includes("```json")) {
+		return "ジェイソンのコード";
+	}
+	if (messageContent.includes("```js") || messageContent.includes("```javascript")) {
+		return "ジャバスクリプトのコード";
+	}
+	if (messageContent.includes("```java")) {
+		return "ジャバのコード";
+	}
+	if (messageContent.includes("```ts") || messageContent.includes("```typescript")) {
+		return "タイプスクリプトのコード";
+	}
+	if (messageContent.includes("```html")) {
+		return "htmlのコード";
+	}
+	if (messageContent.includes("```css")) {
+		return "cssのコード";
+	}
+	if (messageContent.includes("```c")) {
+		return "C言語のコード";
+	}
+	if (messageContent.includes("```python") || messageContent.includes("```py")) {
+		return "パイソンのコード";
+	}
+	if (messageContent.includes("`") || messageContent.includes("```")) {
+		return "コードブロック";
+	}
 	if (messageContent.includes("http")) {
 		if (messageContent.includes("youtube")) {
 			return "ユーチューブのURL";
