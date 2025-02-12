354a72a144783d2ce1eace1d0bfa93ae59915ec0
kasumi-sou <110779036+Sxwako@users.noreply.github.com>
2025-02-13 03:22:26 +0900

    fix: change require file name

--- src/reply/index.js
+++ src/reply/index.js
@@ -1,5 +1,5 @@
 const owata = require("./owata.js");
-const kenkou = require("./kenkouniYokunai.js");
+const kenkou = require("./replyImages.js");
 
 module.exports = async function(message) {
 	await kenkou(message);
