481e3919647f43b5bfe2e6d6c987da9cc35126ce
kasumi-sou <110779036+Sxwako@users.noreply.github.com>
2025-02-13 03:21:58 +0900

    feat: change file name and add reply images

--- src/reply/replyImages.js
+++ src/reply/replyImages.js
@@ -2,4 +2,7 @@ module.exports = async function(message) {
 	if (message.content.includes("健康によくない") || message.content.includes("健康に良くない")) {
 		await message.channel.send({ files: ["https://i.imgur.com/fCAbCPu.png"] });
 	}
+	if (message.content.includes("そうはならんやろ")) {
+		await message.channel.send({ files: ["https://gyazo.com/a90d3aa5edf71c5e132f72d8a8254ce8/max_size/1000"] });
+	}
 };
--- src/voice/tts/generateAudio.js
+++ src/voice/tts/generateAudio.js
@@ -9,7 +9,7 @@ module.exports = async function readMessages(message, messageContent) {
 	// const soundPath = `sounds/${message.author.id}.wav`;
 	// const soundPath = `sounds/${message.id}.wav`;
 	// const defaultVoice = "14";
-	const defaultVoice = "6";
+	const defaultVoice = "14";
 	const VoiceMap = new Map;
 	let voice = VoiceMap.get(message.author.id);
 	if (!voice) {
