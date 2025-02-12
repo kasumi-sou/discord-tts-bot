c08b7714a266d2f7f1d673dccda97daebd2f7ce6
kasumi-sou <110779036+Sxwako@users.noreply.github.com>
2025-02-13 02:04:26 +0900

    change: reply image url

--- src/reply/kenkouniYokunai.js
+++ src/reply/kenkouniYokunai.js
@@ -1,5 +1,5 @@
 module.exports = async function(message) {
 	if (message.content.includes("健康によくない") || message.content.includes("健康に良くない")) {
-		await message.channel.send({ files: ["https://media.discordapp.net/attachments/957153432526000189/1338960239692873798/kenkouni_yokunai.png?ex=67acfb65&is=67aba9e5&hm=b8804ff66df089ac6e19d183beddb15cd8f0c3b5cecd1c0cb7f863adcd3da0ca&=&format=webp&quality=lossless"] });
+		await message.channel.send({ files: ["https://i.imgur.com/fCAbCPu.png"] });
 	}
 };
