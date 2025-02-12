9329de62665f09262c50378a54f662f151faab7c
kasumi-sou <110779036+Sxwako@users.noreply.github.com>
2025-02-13 03:27:53 +0900

    change: send image URL not send image

--- src/reply/replyImages.js
+++ src/reply/replyImages.js
@@ -1,8 +1,8 @@
 module.exports = async function(message) {
 	if (message.content.includes("健康によくない") || message.content.includes("健康に良くない")) {
-		await message.channel.send({ files: ["https://i.imgur.com/fCAbCPu.png"] });
+		await message.channel.send("https://i.imgur.com/fCAbCPu.png");
 	}
 	if (message.content.includes("そうはならんやろ")) {
-		await message.channel.send({ files: ["https://gyazo.com/a90d3aa5edf71c5e132f72d8a8254ce8/max_size/1000"] });
+		await message.channel.send("https://gyazo.com/a90d3aa5edf71c5e132f72d8a8254ce8/max_size/1000");
 	}
 };
