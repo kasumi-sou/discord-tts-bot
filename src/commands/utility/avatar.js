c6be85fa1e311a5e9d24de18b9225a01782b83b9
kasumi-sou <110779036+Sxwako@users.noreply.github.com>
2025-02-17 19:38:47 +0900

    fix: remove unused module

--- src/commands/utility/avatar.js
+++ src/commands/utility/avatar.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, ALLOWED_SIZES } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
