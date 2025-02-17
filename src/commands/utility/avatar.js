e776e630fa0cc328d7104c16719b7b3b66b7bbd9
kasumi-sou <110779036+Sxwako@users.noreply.github.com>
2025-02-17 19:31:34 +0900

    feat: send member's avatar using "/avatar"

---
+++ src/commands/utility/avatar.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder, EmbedBuilder, ALLOWED_SIZES } = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("avatar")
+		.setDescription("アバター画像を返します")
+		.addUserOption(option =>
+			option.setName("user")
+				.setDescription("ユーザを選択してください")
+				.setRequired(true),
+		),
+	/**
+   * @type {(interaction: import("discord.js").CommandInteraction) => Promise<void>}
+   */
+	async execute(interaction) {
+		const member = interaction.options.getMember("user");
+		const command = new EmbedBuilder()
+			.setColor(0xffdbed)
+			.setTitle(`${member.displayName} のアバター`)
+			// .setURL(member.displayAvatarURL())
+			.setImage(member.displayAvatarURL({ size: 1024 }));
+
+		await interaction.reply({ embeds: [command] });
+	},
+
+};
