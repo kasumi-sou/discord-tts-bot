acf4a96822e5644928e5b5bba1893a7247d9a6fd
kasumi-sou <110779036+Sxwako@users.noreply.github.com>
2025-02-21 02:49:04 +0900

    fix: stop process properly

--- src/commands/voice/setSpeed.js
+++ src/commands/voice/setSpeed.js
@@ -10,7 +10,7 @@ module.exports = {
 		.addStringOption((option) => (
 			option
 				.setName("speed")
-				.setDescription("0.3 ~ 5の間で指定してください")
+				.setDescription("0.3 ~ 5の間で指定してください(デフォルト値: 1 )")
 				.setRequired(true)
 		)),
 	/**
@@ -23,6 +23,7 @@ module.exports = {
 		// eslint-disable-next-line yoda
 		if (speed < 0.3 || 5 < speed) {
 			await interaction.reply(":warning: **値が不正です！**");
+			return;
 		}
 		const memberId = interaction.member.id;
 		userData.set(memberId, { speed });
