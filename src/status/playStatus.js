7257ec85b0d8d61f40e7d682bd002bf9a67e23ac
kasumi-sou <110779036+Sxwako@users.noreply.github.com>
2025-02-24 23:47:59 +0900

    feat: change custom status at regular intervals

--- src/status/playStatus.js
+++ src/status/playStatus.js
@@ -1,11 +1,31 @@
 "use strict";
 
 const { Events, ActivityType } = require("discord.js");
+const { setTimeout: sleep } = require("timers/promises");
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
-		client.user.setActivity("お昼寝", { type: ActivityType.Competing });
+	async execute(client) {
+		while (true) {
+
+			await client.user.setActivity({
+				name: "✅ /joinで参加",
+				type: ActivityType.Custom,
+			});
+			await sleep(10000);
+
+			await client.user.setActivity({
+				name: "🎉 v0.2.0! 新機能は/helpから",
+				type: ActivityType.Custom,
+			});
+			await sleep(10000);
+
+			await client.user.setActivity({
+				name: "✅ オープンソースbot",
+				type: ActivityType.Custom,
+			});
+			await sleep(10000);
+		}
 	},
 };
