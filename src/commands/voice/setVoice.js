284be734693f3eebfa1556d9dcf8dfb94df9b92b
kasumi-sou <110779036+Sxwako@users.noreply.github.com>
2025-03-09 00:28:00 +0900

    feat: reading using Aivis Speech
    - add option "engine" in "/set_voice" command

--- src/commands/voice/setVoice.js
+++ src/commands/voice/setVoice.js
@@ -2,7 +2,8 @@
 
 const { SlashCommandBuilder, Events } = require("discord.js");
 const { default: axios } = require("axios");
-const rpc = axios.create({ baseURL: "http://localhost:50021", proxy: false });
+const rpcVoiceVox = axios.create({ baseURL: "http://localhost:50021", proxy: false });
+const rpcAivis = axios.create({ baseURL: "http://localhost:10101", proxy: false });
 const { user: userData } = require("../../data");
 
 module.exports = {
@@ -12,6 +13,16 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("set_voice")
 		.setDescription("キャラクターと話し方を選択します")
+		.addStringOption((option) => (
+			option
+				.setName("engine")
+				.setDescription("読み上げエンジンを選択します (代表的なキャラクター: VoiceVox: 四国めたん,ずんだもん など)")
+				.addChoices(
+					{ name: "VoiceVox", value: "voiceVox" },
+					{ name: "AivisSpeech", value: "aivis" },
+				)
+				.setRequired(true)
+		))
 		.addStringOption((option) => (
 			option
 				.setName("chara")
@@ -31,13 +42,17 @@ module.exports = {
    */
 	async autocomplete(interaction) {
 
-		let choices = null;
 
-		// API叩いてキャラ一覧を取得
-		const charaMap = await getChara();
+		// 以下でAPI叩いてキャラ一覧を取得
+		// voicevoxとaivisで条件分岐
+		const selectedEngine = interaction.options.getString("engine");
+
+		const charaMap = selectedEngine === "voiceVox" ? await getCharaVoiVo() : selectedEngine === "aivis" ? await getCharaAivis() : null;
 
 		// コマンド実行者が現在設定中のオプションを取得
 		const focusedOption = interaction.options.getFocused(true);
+
+		let choices = null;
 
 		if (focusedOption.name === "chara") {
 			// キャラを設定中だったら
@@ -56,6 +71,9 @@ module.exports = {
 
 		// djsの仕様上25までしか選択肢を表示できないので最初から25個のみを表示
 		const filtered = choices.filter(choice => choice.startsWith(focusedOption.value)).slice(0, 25);
+
+		// choicesがnullやundefinedのときはスルー
+		if (!choices || choices.length === 0) {return;}
 		await interaction.respond(
 			// 一つ上で作った配列を返す
 			filtered.map(choice => ({ name: choice, value: choice })),
@@ -67,13 +85,12 @@ module.exports = {
 	async execute(interaction) {
 		const memberId = interaction.member.id;
 
-		const charaMap = await getChara();
-
 		// 入力情報取得
 		const selectedChara = interaction.options.getString("chara");
 		const selectedStyle = interaction.options.getString("style");
+		const selectedEngine = interaction.options.getString("engine");
 
-		// 省略
+		const charaMap = selectedEngine === "voiceVox" ? await getCharaVoiVo() : selectedEngine === "aivis" ? await getCharaAivis() : null;
 		const charaObj = charaMap.find(chara => (chara.name === selectedChara));
 		const styleObj = charaObj.styles.find(style => (style.name === selectedStyle));
 		const styleId = styleObj.id;
@@ -87,20 +104,38 @@ module.exports = {
 };
 
 // APIからキャラ一覧を取得する関数
-let charaCache = null;
-async function getChara() {
-	if (charaCache) {return charaCache;}
-	const coreVersions = await rpc.get("core_versions", {
+let charaCacheVoiVo = null;
+async function getCharaVoiVo() {
+	if (charaCacheVoiVo) {return charaCacheVoiVo;}
+	const coreVersions = await rpcVoiceVox.get("core_versions", {
 		heders: {
 			"accept": "application/json",
 		},
 	});
 	// console.log(coreVersions.data.toString());
-	const speakerList = await rpc.get(`speakers?core_version=${coreVersions.data.toString()}`, {
+	const speakerList = await rpcVoiceVox.get(`speakers?core_version=${coreVersions.data.toString()}`, {
 		heders: {
 			"accept": "application/json",
 		},
 	});
 	// console.log(speakerList.data);
-	return charaCache = speakerList.data;
+	return charaCacheVoiVo = speakerList.data;
+};
+
+let charaCacheAivis = null;
+async function getCharaAivis() {
+	if (charaCacheAivis) {return charaCacheAivis;}
+	const coreVersions = await rpcAivis.get("core_versions", {
+		heders: {
+			"accept": "application/json",
+		},
+	});
+	// console.log(coreVersions.data.toString());
+	const speakerList = await rpcAivis.get(`speakers?core_version=${coreVersions.data.toString()}`, {
+		heders: {
+			"accept": "application/json",
+		},
+	});
+	// console.log(speakerList.data);
+	return charaCacheAivis = speakerList.data;
 };
