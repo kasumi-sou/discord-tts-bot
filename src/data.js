5ad5f906fd5408da6ede9084c644d19bc772ef0e
kasumi-sou <110779036+Sxwako@users.noreply.github.com>
2025-03-19 00:42:51 +0900

    feat: new method of get/set dictionary data

--- src/data.js
+++ src/data.js
@@ -4,7 +4,10 @@ const fs = require("node:fs");
 
 const guildMap = new Map();
 let userMap = new Map();
+const dictMap = new Map();
+
 const userPath = "./.data/user.json";
+const getGuildDictPath = (guildId) => `./.data/dictionary/${guildId}.json`;
 
 if (!fs.existsSync("./.data")) {
   fs.mkdirSync("./.data");
@@ -48,6 +51,31 @@ module.exports = {
       const result = userMap.set(key, { ...settings, ...value });
       const userArray = Array.from(userMap.entries());
       fs.writeFileSync(userPath, JSON.stringify(userArray), "utf-8");
+      return result;
+    },
+  },
+  dict: {
+    get(guilId) {
+      const getDictMap = dictMap.get(guilId);
+      if (getDictMap) {
+        const dict = Object.freeze(getDictMap);
+        return dict;
+      }
+      else if (fs.existsSync(getGuildDictPath(guilId))) {
+        const dictJson = fs.readFileSync(getGuildDictPath(guilId), "utf-8");
+        const dict = JSON.parse(dictJson);
+        dictMap.set(guilId, dict);
+        return Object.freeze(dict);
+      }
+      else {return [];}
+    },
+    set(guildId, value) {
+      const dict = module.exports.dict.get(guildId);
+
+      const newDict = [...dict.filter(entry => value.every(newEntry => newEntry.word !== entry.word)), ...value];
+
+      const result = dictMap.set(guildId, newDict);
+      fs.writeFileSync(getGuildDictPath(guildId), JSON.stringify(newDict), "utf-8");
       return result;
     },
   },
