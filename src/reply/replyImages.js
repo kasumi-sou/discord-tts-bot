816ed96acc79fa434d2e7b044fdfb2d1a0c3b23f
kasumi-sou <110779036+Sxwako@users.noreply.github.com>
2025-04-22 22:30:58 +0900

    feat: reply message when mentioned

--- src/reply/replyImages.js
+++ src/reply/replyImages.js
@@ -1,10 +1,15 @@
 "use strict";
 
+const { clientId } = require("./../../config.json");
+
 module.exports = async function(message) {
+  if (message.content.includes(`<@${clientId}>`)) {
+    await message.reply("はにゃ(੭ ᐕ))？");
+  }
   if (message.content.includes("健康によくない") || message.content.includes("健康に良くない")) {
-    await message.channel.send("https://i.imgur.com/fCAbCPu.png");
+    await message.reply("https://i.imgur.com/fCAbCPu.png");
   }
   if (message.content.includes("そうはならんやろ")) {
-    await message.channel.send("https://gyazo.com/a90d3aa5edf71c5e132f72d8a8254ce8/max_size/1000");
+    await message.reply("https://gyazo.com/a90d3aa5edf71c5e132f72d8a8254ce8/max_size/1000");
   }
 };
